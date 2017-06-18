5617ba37041c6c5f0538679758247f2a379dbb6d
andresattler <andresattler.webdev@gmail.com>
2017-06-18 22:33:14 +1200

    basic functionality

--- dist/index.js
+++ dist/index.js
@@ -19,7 +19,41 @@ app.get('/webhook', function (req, res) {
     res.sendStatus(403);
   }
 });
-app.listen(process.env.PORT || 8000
 
-// tocken advekcw4t93409tuqw
-);
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  // eslint-disable-next-line no-console
+  console.log('Message data: ', event.message);
+}
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function (entry) {
+      // const pageID = entry.id
+      // const timeOfEvent = entry.time
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function (event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          // eslint-disable-next-line no-console
+          console.log('Webhook received unknown event: ', event);
+        }
+      });
+    }
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    );res.sendStatus(200);
+  }
+});
+
+app.listen(process.env.PORT || 8000);
