4436e9842169af514968ed71d3fb88682edb27e5
andresattler <andresattler.webdev@gmail.com>
2017-06-18 23:03:45 +1200

    added bodyparser for json

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,15 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+
+app.use(_bodyParser2.default.json());
 
 app.get('/webhook', function (req, res) {
   if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'advekcw4t93409tuqw') {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+app.use(bodyParser.json())
 
 app.get('/webhook', (req, res) => {
   if (req.query['hub.mode'] === 'subscribe' &&
