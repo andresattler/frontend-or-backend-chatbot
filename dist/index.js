f456e1aceeef273bc223b58b889f16103f5216a4
andresattler <andresattler.webdev@gmail.com>
2017-06-18 22:01:01 +1200

    added webhook

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
-app.get('/', function (req, res) {
+app.get('/webhook', function (req, res) {
   res.send('hello World');
 });
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8000
+
+// tocken advekcw4t93409tuqw
+);
