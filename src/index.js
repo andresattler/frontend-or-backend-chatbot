ccc1dd95e59934ff513239eed1f83a6c5b45d2a7
andresattler <andresattler.webdev@gmail.com>
2017-06-18 21:43:30 +1200

    added webhook

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import express from 'express'
 const app = express()
 
 
-app.get('/', (req, res) => {
+app.get('/webhook', (req, res) => {
   res.send('hello World')
 })
 
 app.listen(process.env.PORT || 8000)
+
+// tocken advekcw4t93409tuqw
