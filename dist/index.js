caf29114bf53db733ec9c5c754c47013998c541d
andresattler <andresattler.webdev@gmail.com>
2017-06-18 22:11:29 +1200

    added verification

--- dist/index.js
+++ dist/index.js
@@ -9,9 +9,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 
 app.get('/webhook', function (req, res) {
-  res.send('hello World');
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'advekcw4t93409tuqw') {
+    // eslint-disable-next-line no-console
+    console.log('Validating webhook');
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('Failed validation. Make sure the validation tokens match.');
+    res.sendStatus(403);
+  }
 });
-
 app.listen(process.env.PORT || 8000
 
 // tocken advekcw4t93409tuqw
--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ const app = express()
 
 
 app.get('/webhook', (req, res) => {
-  res.send('hello World')
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'advekcw4t93409tuqw') {
+    // eslint-disable-next-line no-console
+    console.log('Validating webhook')
+    res.status(200).send(req.query['hub.challenge'])
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('Failed validation. Make sure the validation tokens match.')
+    res.sendStatus(403)
+  }
 })
-
 app.listen(process.env.PORT || 8000)
 
 // tocken advekcw4t93409tuqw
