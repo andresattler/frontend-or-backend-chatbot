c74caec0ccb135dcc1e02ac5dfb8bbdfd1e5658f
andresattler <andresattler.webdev@gmail.com>
2017-06-25 17:21:17 +1200

    changed result message

--- dist/api/responses.js
+++ dist/api/responses.js
@@ -3,14 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var welcomeMessage = exports.welcomeMessage = 'Welcome can I help yout of find out if You are more the frontend guy or more a backend evangelists!';
+var welcomeMessage = exports.welcomeMessage = 'Welcome can I help yout of find out if You are more the frontend guy or more a backend evangelists!?';
 
 var notUnderstood = exports.notUnderstood = 'Sory I could not understand you';
 
 var rejectionMessage = exports.rejectionMessage = 'Ok then not :) I wish you a nice day!';
 
-var questions = exports.questions = [{ text: 'first question', weight: 1 }, { text: 'question 2', weight: 1 }, { text: 'question 3', weight: 1 }, { text: 'question 4', weight: 1 }, { text: 'question 5', weight: 1 }];
+var questions = exports.questions = [{ text: 'Do you like to ', weight: 1 }, { text: 'question 2', weight: 1 }, { text: 'question 3', weight: 1 }, { text: 'question 4', weight: 1 }, { text: 'question 5', weight: 1 }];
 
 var result = exports.result = function result(res) {
-  return 'I think you are more the ' + res + ' guy';
+  return 'The data says that your problay mor a ' + res + ' guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!';
 };
--- src/api/responses.js
+++ src/api/responses.js
@@ -1,16 +1,16 @@
 
-export const welcomeMessage = 'Welcome can I help yout of find out if You are more the frontend guy or more a backend evangelists!'
+export const welcomeMessage = 'Welcome can I help yout of find out if You are more the frontend guy or more a backend evangelists!?'
 
 export const notUnderstood = 'Sory I could not understand you'
 
 export const rejectionMessage = 'Ok then not :) I wish you a nice day!'
 
 export const questions = [
-  { text: 'first question', weight: 1 },
+  { text: 'Do you like to ', weight: 1 },
   { text: 'question 2', weight: 1 },
   { text: 'question 3', weight: 1 },
   { text: 'question 4', weight: 1 },
   { text: 'question 5', weight: 1 },
 ]
 
-export const result = res => `I think you are more the ${res} guy`
+export const result = res => `The data says that your problay mor a ${res} guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!`
