d10ab4d4511ad65f45c8fab0fcd67478cb282ddb
andresattler <andresattler.webdev@gmail.com>
2017-06-27 19:58:30 +1200

    written basic questions

--- dist/api/responses.js
+++ dist/api/responses.js
@@ -9,8 +9,8 @@ var notUnderstood = exports.notUnderstood = 'Sory I could not understand you';
 
 var rejectionMessage = exports.rejectionMessage = 'Ok then not :) I wish you a nice day!';
 
-var questions = exports.questions = [{ text: 'Do you like to ', weight: 1 }, { text: 'question 2', weight: 1 }, { text: 'question 3', weight: 1 }, { text: 'question 4', weight: 1 }, { text: 'question 5', weight: 1 }];
+var questions = exports.questions = [{ text: 'Do you like to create user intefaces? ', weight: 1 }, { text: 'Are you interested in complex algorythms processing data?', weight: -1 }, { text: 'Do you want to work with Databases?', weight: -1 }, { text: 'Do you like to visually create?', weight: 1 }];
 
 var result = exports.result = function result(res) {
-  return 'The data says that your problay mor a ' + res + ' guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!';
+  return 'The data says that your probably mor a ' + res + ' guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!';
 };
