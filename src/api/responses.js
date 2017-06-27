d645141b128d0d57a24d4f5e977e99f235b30359
andresattler <andresattler.webdev@gmail.com>
2017-06-27 19:54:55 +1200

    written basic questions

--- src/api/responses.js
+++ src/api/responses.js
@@ -6,11 +6,10 @@ export const notUnderstood = 'Sory I could not understand you'
 export const rejectionMessage = 'Ok then not :) I wish you a nice day!'
 
 export const questions = [
-  { text: 'Do you like to ', weight: 1 },
-  { text: 'question 2', weight: 1 },
-  { text: 'question 3', weight: 1 },
-  { text: 'question 4', weight: 1 },
-  { text: 'question 5', weight: 1 },
+  { text: 'Do you like to create user intefaces? ', weight: 1 },
+  { text: 'Are you interested in complex algorythms processing data?', weight: -1 },
+  { text: 'Do you want to work with Databases?', weight: -1 },
+  { text: 'Do you like to visually create?', weight: 1 },
 ]
 
-export const result = res => `The data says that your problay mor a ${res} guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!`
+export const result = res => `The data says that your probably mor a ${res} guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!`
