a5c32a6e5c425276d6f6073ebe55308b390ec715
andresattler <andresattler.webdev@gmail.com>
2017-06-27 20:08:14 +1200

    corrected spelling

--- dist/api/responses.js
+++ dist/api/responses.js
@@ -3,14 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var welcomeMessage = exports.welcomeMessage = 'Welcome can I help yout of find out if You are more the frontend guy or more a backend evangelists!?';
+var welcomeMessage = exports.welcomeMessage = 'Welcome can I help you of find out if You are more the frontend guy or more a backend evangelists!?';
 
-var notUnderstood = exports.notUnderstood = 'Sory I could not understand you';
+var notUnderstood = exports.notUnderstood = 'Sorry I could not understand you';
 
 var rejectionMessage = exports.rejectionMessage = 'Ok then not :) I wish you a nice day!';
 
-var questions = exports.questions = [{ text: 'Do you like to create user intefaces? ', weight: 1 }, { text: 'Are you interested in complex algorythms processing data?', weight: -1 }, { text: 'Do you want to work with Databases?', weight: -1 }, { text: 'Do you like to visually create?', weight: 1 }];
+var questions = exports.questions = [{ text: 'Do you like to create user interfaces? ', weight: 1 }, { text: 'Are you interested in complex algorithms processing data?', weight: -1 }, { text: 'Do you want to work with Databases?', weight: -1 }, { text: 'Do you like to visually create?', weight: 1 }];
 
 var result = exports.result = function result(res) {
-  return 'The data says that your probably mor a ' + res + ' guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!';
+  return 'The data says that your probably more a ' + res + ' guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for everyone to find out!';
 };
--- src/api/responses.js
+++ src/api/responses.js
@@ -1,15 +1,15 @@
 
-export const welcomeMessage = 'Welcome can I help yout of find out if You are more the frontend guy or more a backend evangelists!?'
+export const welcomeMessage = 'Welcome can I help you of find out if You are more the frontend guy or more a backend evangelists!?'
 
-export const notUnderstood = 'Sory I could not understand you'
+export const notUnderstood = 'Sorry I could not understand you'
 
 export const rejectionMessage = 'Ok then not :) I wish you a nice day!'
 
 export const questions = [
-  { text: 'Do you like to create user intefaces? ', weight: 1 },
-  { text: 'Are you interested in complex algorythms processing data?', weight: -1 },
+  { text: 'Do you like to create user interfaces? ', weight: 1 },
+  { text: 'Are you interested in complex algorithms processing data?', weight: -1 },
   { text: 'Do you want to work with Databases?', weight: -1 },
   { text: 'Do you like to visually create?', weight: 1 },
 ]
 
-export const result = res => `The data says that your probably mor a ${res} guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for evryone to find out!`
+export const result = res => `The data says that your probably more a ${res} guy \n but my personal opinion is that you should just pick a language and build stuff. \n I think that is the best way for everyone to find out!`
